1d009d55f30df940b553029e125210569789f3b2
balintklaudia <balintklaudia05@gmail.com>
2019-11-27 11:48:56 +0100

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode jump;
+	public KeyCode throwThings;
+
+	private Rigidbody2D theRB;
+
+	public Transform groundCheckPoint;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+	public bool isGrounded;
+
+	private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+
+		anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+		
+        if(Input.GetKey(left))
+		{
+			theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+		}else if(Input.GetKey(right))
+		{
+			theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+		}else {
+			theRB.velocity = new Vector2(0, theRB.velocity.y);
+		}
+
+		if(Input.GetKeyDown(jump) && isGrounded)
+		{
+			theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+		}
+
+		if(theRB.velocity.x < 0) {
+			transform.localScale = new Vector3(-0.35f, 0.35f, 0.35f);
+		} else if(theRB.velocity.x > 0)
+		{
+			transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+		}
+
+		anim.SetFloat("Speed", Mathf.Abs(theRB.velocity.x));
+		anim.SetBool("Grounded", isGrounded);
+
+    }
+}
