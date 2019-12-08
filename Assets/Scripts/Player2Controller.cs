6b36953a1353fb3b34dd02249df96333d1557514
Bálint Simon <simon.blnt93@gmail.com>
2019-12-08 17:59:26 +0100

    Sensor working, no added force tho.

---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player2Controller : MonoBehaviour
+{
+
+	public float moveSpeed;
+	public float jumpForce;
+
+
+	private Rigidbody2D theRB;
+
+	public Transform groundCheckPoint;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+	public bool isGrounded;
+
+	private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+
+		anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		/*
+		var dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+		
+		isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+		
+        if(dirX < 0)
+		{
+			theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+		}else if(dirX > 0)
+		{
+			theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+		}else {
+			theRB.velocity = new Vector2(0, theRB.velocity.y);
+		}
+
+		if(CrossPlatformInputManager.GetButtonDown("Jump") && isGrounded)
+		{
+			theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+		}
+
+		if(theRB.velocity.x < 0) {
+			transform.localScale = new Vector3(-0.35f, 0.35f, 0.35f);
+		} else if(theRB.velocity.x > 0)
+		{
+			transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+		}
+
+		anim.SetFloat("Speed", Mathf.Abs(theRB.velocity.x));
+		anim.SetBool("Grounded", isGrounded);
+*/
+    }
+}
---
+++ Assets/Scripts/SensorGrabberScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SensorGrabberScript : MonoBehaviour
+{
+	public bool grabbed;
+	RaycastHit2D hit;
+	public float distance = 2f;
+	public Transform holdPoint;
+	public float throwforce;
+	public LayerMask notgrabbed;
+	public float sensorForce;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {	
+		sensorForce = Input.acceleration.x;
+        if(CrossPlatformInputManager.GetButtonDown("Interact"))
+		{
+			if (!grabbed)
+			{
+				Physics2D.queriesStartInColliders=false;
+
+				hit = Physics2D.Raycast(transform.position, Vector2.right*transform.localScale.x, distance);
+				if (hit.collider != null && hit.collider.tag == "Grabbable")
+				{
+					grabbed = true;
+				}
+			}
+			if (grabbed)
+			{
+				grabbed = true;
+			}
+		}
+
+		if (grabbed)
+		{
+			if (sensorForce > 0.3f || sensorForce < -0.3f)
+			{
+				grabbed=false;
+				
+				if(hit.collider.gameObject.GetComponent<Rigidbody2D>()!=null)
+				{
+					hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity= new Vector2(transform.localScale.x,1)*throwforce;
+				}
+			}
+			else
+			{
+					hit.collider.gameObject.transform.position = holdPoint.position;
+			}
+		}
+    }
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(transform.position,transform.position+Vector3.right*transform.localScale.x*distance);
+	}
+}
